c0ac14cbabf24a51c64d21bbbae93d95c18e8589
Joey Territo <jtt9340@rit.edu>
2020-05-26 20:10:54 -0400

    Added my own custom theme which is a variation of the Avit theme that ships with Oh-My-Zsh

--- Library/Preferences/net.sourceforge.zsh.Zsh/.oh-my-zsh/custom/aliases.zsh
+++ Library/Preferences/net.sourceforge.zsh.Zsh/.oh-my-zsh/custom/aliases.zsh
@@ -5,11 +5,11 @@
 alias ls="ls -abFG"
 alias l="ls -l"
 # alias glgg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-alias bfg="java -jar /usr/local/bin/bfg-1.13.0.jar"
 
 # Make some commands a little more verbose
 alias rm="rm -v"
 alias mv="mv -v"
+alias cp="cp -v"
 alias diff="diff -s"
 
 # Use the version of Vim installed by Homebrew, not the one Apple shipped
