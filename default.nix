5fcaf3b42c9441a95eab263d91d993c18cb7726d
Joey Territo <jtt9340@rit.edu>
2021-08-03 23:38:20 -0400

    Update dotdrop on NixOS to v1.5.4

--- default.nix
+++ default.nix
@@ -7,23 +7,39 @@ let
     let
       dotdrop = python39.pkgs.buildPythonPackage rec {
         pname = "dotdrop";
-        version = "1.5.3";
+        version = "1.5.4";
 
         src = fetchFromGitHub {
           owner = "deadc0de6";
           repo = pname;
           rev = "v${version}";
-          sha256 = "1qqlh9pznssbgg45qvxw73j4ii1hl1y118lyaldblmjqhdc65768";
+          sha256 = "03bisnmlly83qxjvx3pzf9ijw7hmmqmz043jrbn3aa5kch8yh4qz";
         };
 
+        # TODO: Figure out how to get the unit tests to pass so that
+        # this can be set to true
         doCheck = false;
 
+        # Runtime dependencies
         propagatedBuildInputs = with python39.pkgs; [
           jinja2
           docopt
           ruamel_yaml
           python_magic
         ];
+
+        # Build/test time dependencies
+        # Uncomment when doCheck can be set to true
+        # nativeBuildInputs = with python39.pkgs; [
+        #   pycodestyle
+        #   nose
+        #   coverage
+        #   coveralls
+        #   pyflakes
+        #   pylint
+        #   halo
+        #   file
+        # ];
       };
     in
       python39.withPackages (ps: with ps; [
